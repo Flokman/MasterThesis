580237264fe22615d1c5453f3539a57fcce23d31
F.G.Drost@student.rug.nl <s2934833@peregrine.hpc.rug.nl>
2019-12-10 10:01:34 +0100

    from peregrine

---
+++ Dropout/runDropoutMNIST.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#SBATCH --time=00:10:00
+
+#SBATCH --partition=gpu
+#SBATCH --gres=gpu:v100:1
+#SBATCH --job-name=MCDropout
+#SBATCH --mem=32000
+
+module load Python/3.6.4-foss-2018a
+module load TensorFlow/1.12.0-fosscuda-2018a-Python-3.6.4 
+module load scikit-learn/0.19.1-foss-2018a-Python-3.6.4 
+
+python dropoutmnist.py
