48ecf68cdcaee437e67accb68c3e4c789c30d92f
Folke <folkedrostspam@gmail.com>
2019-12-22 11:45:39 +0100

    Fix for merge

--- Dropout/run_dropout_mes.sh
+++ Dropout/run_dropout_mes.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
-#SBATCH --time=00:35:00
+#SBATCH --time=00:45:00
 
 #SBATCH --partition=gpu
 #SBATCH --gres=gpu:v100:1
 #SBATCH --job-name=MCDropoutMessidor2
 #SBATCH --mem=32000
 
-module load Python/3.6.4-foss-2018a
+module load Python/3.6.4-fosscuda-2018a 
 module load TensorFlow/1.12.0-fosscuda-2018a-Python-3.6.4 
 module load scikit-learn/0.19.1-foss-2018a-Python-3.6.4 
+module load Pillow/5.0.0-foss-2018a-Python-3.6.4
 
 python dropout_mes.py
